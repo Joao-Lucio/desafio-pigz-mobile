49627855bb794e93019825240d7e3a43583370b5
Joao-Lucio <joao.silvarrr@gmail.com>
2023-09-03 00:42:24 -0400

    import fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
 export default function App() {
+
+const [hasLoadedFonts] = useFonts({
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+})
+
+if(!hasLoadedFonts) return null
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
