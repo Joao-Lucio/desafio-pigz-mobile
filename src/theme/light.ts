46c3cc4e6b8714d90024daf642a6295de2b20a38
Joao-Lucio <joao.silvarrr@gmail.com>
2023-09-11 23:02:27 -0400

    Create Theme

---
+++ src/theme/light.ts
@@ -0,0 +1,24 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#FFFFFF',
+    PRIMARY: '#FA641E',
+    SECONDARY: '#FF881F',
+
+    TEXT_PRIMARY: '#676767',
+    TITLE: '#333333',
+    SUB_TITLE: '#222222',
+    TEXT_BUTTON: '#FFF',
+    TEXT_BUTTON_SECONDARY: '#676767',
+    LINE: '#676767',
+  },
+
+  FONTS: {
+    POPPINS_REGULAR: "Poppins_400Regular",
+    POPPINS_MEDIUM: "Poppins_500Medium",
+    POPPINS_SEMI_BOLD: 'Poppins_600SemiBold',
+    POPPINS_BOLD: "Poppins_700Bold",
+    ROBOTO_REGULAR: "Roboto_400Regular",
+    ROBOTO_MEDIUM: "Roboto_500Medium",
+    ROBOTO_BOLD: "Roboto_700Bold",
+  },
+}
