8e1c0e563cd95609e63def2d639c653405626a7c
Joao-Lucio <joao.silvarrr@gmail.com>
2023-09-14 04:16:52 -0400

    Add new components

--- src/components/Global/styles.ts
+++ src/components/Global/styles.ts
@@ -48,3 +48,49 @@ font-size:40px;
 color: ${({theme}) => theme.COLORS.TITLE_CONTAINER};
 align-self:center;
 `
+
+export const TextDeliveryInfo = styled.Text`
+font-family: ${({theme}) => theme.FONTS.POPPINS_REGULAR};
+font-size: 14px;
+color: ${({theme}) => theme.COLORS.DELIVERY_INFO};
+`
+
+export const TextDeliveryValor = styled.Text<{$type?: string}>`
+font-family: ${({theme}) => theme.FONTS.POPPINS_BOLD};
+font-size: ${props => props.$type ? '35px' : '20px'};
+color: ${({theme}) => theme.COLORS.PRIMARY};
+`
+
+export const Line = styled.View`
+height: 1px;
+background-color: ${({theme}) => theme.COLORS.LINE};
+margin-top: 12px;
+margin-left: 8px;
+`
+
+export const LineVerticalSmall = styled.View`
+width: 2px;
+height: 20px;
+background-color: ${({theme}) => theme.COLORS.LINE};
+`
+
+export const LineVerticalBig = styled(LineVerticalSmall)`
+height: 40px;
+`
+
+export const Percurso = styled.Text`
+font-family: ${({theme}) => theme.FONTS.ROBOTO_REGULAR};
+font-size: 12px;
+color: ${({theme}) => theme.COLORS.TEXT_BUTTON};
+`
+
+export const TextDelivery = styled(Percurso)`
+font-family: ${({theme}) => theme.FONTS.ROBOTO_MEDIUM};
+font-size: 18px;
+`
+
+export const TextDeliveryStatus = styled.Text`
+font-family:${({theme}) => theme.FONTS.POPPINS_MEDIUM};
+font-size:18px;
+color:${({theme}) => theme.COLORS.PRIMARY};
+`
