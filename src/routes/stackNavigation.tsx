d58042668a975960ca98b49d19ee7bc1bd0a2132
Joao-Lucio <joao.silvarrr@gmail.com>
2023-09-14 04:25:00 -0400

    Delete globalStyle.ts

--- src/routes/stackNavigation.tsx
+++ src/routes/stackNavigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
-import styleGlobal from '../../globalStyle'
+import theme from '../theme/light'
 
 import {SignIn} from '../screens/SignIn'
 import TabNavigation from './tabNavigation'
@@ -13,7 +13,7 @@ export function StackNavigation(){
     <Navigator >
       <Screen name='SignIn' component={SignIn} options={{headerShown: false}}/>
       <Screen name='Navigation' component={TabNavigation} options={{headerShown:false}}/>
-      <Screen name='NewDelivery' component={NewDelivery} options={{headerTintColor:'#FA641F',headerTitleAlign:'center',headerTitle:'Nova entrega',headerTitleStyle:{fontFamily: styleGlobal.fonts.medium,fontSize:18,color:'#222222'}}}/>
+      <Screen name='NewDelivery' component={NewDelivery} options={{headerTintColor:theme.COLORS.PRIMARY,headerTitleAlign:'center',headerTitle:'Nova entrega',headerTitleStyle:{fontFamily: theme.FONTS.POPPINS_MEDIUM,fontSize:18,color:'#222222'}}}/>
     </Navigator>
   )
 }
--- src/routes/tabNavigation.tsx
+++ src/routes/tabNavigation.tsx
@@ -5,7 +5,7 @@ import Report from '../../assets/report.svg'
 import Profile from '../../assets/profile.svg'
 import Overview from '../../assets/home.svg'
 import Back from '../../assets/arrow-back.svg'
-import styleGlobal from '../../globalStyle'
+import theme from '../theme/light'
 
 import {Home} from '../screens/Home'
 
@@ -18,11 +18,11 @@ export default function TabNavigation(){
     screenOptions={{
       tabBarActiveTintColor: "#FA641E",
       tabBarLabelStyle: {
-        fontFamily: styleGlobal.fonts.medium,
+        fontFamily: theme.FONTS.POPPINS_MEDIUM,
       }
     }}>
       <Screen name='Relatorio' component={Home} options={{headerTitleAlign:'center',tabBarIcon: Report}}/>
-      <Screen name='Visão Geral' component={Home} options={{headerTitleAlign:'center', headerTitleStyle:{fontFamily: styleGlobal.fonts.medium,fontSize:18,color:'#222222'},tabBarIcon: Overview, headerLeft: ()=>(
+      <Screen name='Visão Geral' component={Home} options={{headerTitleAlign:'center', headerTitleStyle:{fontFamily: theme.FONTS.POPPINS_MEDIUM,fontSize:18,color:'#222222'},tabBarIcon: Overview, headerLeft: ()=>(
         <Back style={{marginLeft: 16, marginTop:4}}/>
       )}}/>
       <Screen name='Perfil' component={Home} options={{headerTitleAlign:'center',tabBarIcon: Profile}}/>
