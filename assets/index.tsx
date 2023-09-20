6934033bbde770ac5c731fde7d0e037bbe4f34ae
Joao-Lucio <joao.silvarrr@gmail.com>
2023-09-19 23:01:55 -0400

    added index file to export svg

---
+++ assets/index.tsx
@@ -0,0 +1,23 @@
+import LogoPigz from './pigz-logotipo.svg'
+import LogoGoogle from './logo-google.svg'
+import ArrowBack from './arrow-back.svg'
+import Check from './check.svg'
+import Close from './close.svg'
+import Delivery from './delivery.svg'
+import Home from './home.svg'
+import Profile from './profile.svg'
+import Qrcode from './qrcode.svg'
+import Report from './report.svg'
+
+export default{
+  LogoPigz,
+  LogoGoogle,
+  ArrowBack,
+  Check,
+  Close,
+  Delivery,
+  Home,
+  Profile,
+  Qrcode,
+  Report
+}
